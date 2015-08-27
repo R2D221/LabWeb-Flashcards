18cc69dc20176ab704428a23cb2ba878b1aa8e53
Jesús Herrera <chuu24@gmail.com>
2015-08-27 01:23:42 -0500

    Update training-plan modification

--- js/bonusQuestion.js
+++ js/bonusQuestion.js
@@ -1,6 +1,54 @@
-var answers;
+var answers = [ "aguascalientes",
+        "mexicali",
+        "la paz",
+        "campeche",
+        "saltillo",
+        "colima",
+        "tuxtla gutierrez",
+        "chihuahua",
+        "durango",
+        "guanajuato",
+        "chilpancingo",
+        "pachuca",
+        "guadalajara",
+        "toluca",
+        "morelia",
+        "cuernavaca",
+        "tepic",
+        "monterrey",
+        "oaxaca",
+        "puebla",
+        "queretaro",
+        "chetumal",
+        "san luis potosi",
+        "culiacan",
+        "hermosillo",
+        "villahermosa",
+        "ciudad victoria",
+        "tlaxcala",
+        "xalapa",
+        "merida",
+        "zacatecas"];
 
 function checkAnswer(){
-    
+    var answ = $('#answer').val();
+    if(answers.indexOf(answ) > -1){
+        var ind = answers.indexOf(answ)+ 1;
+        document.getElementById('answer').value = '';
+        $("#" + ind).html("<b>" + answ + "</b>");
+    }
 }
 
+function loadJSON() {
+    $.getJSON( "http://paginas.ccm.itesm.mx/~A01215142/Flashcards/bonus-1.json", function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+            items.push( "<li id='" + key + "'>" + val + "</li>" );
+        });
+
+        $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+        }).appendTo( "body" );
+    });
+}
