31c92bfbc5c9e4927c6d6ee1049bdb75c3de166c
Jorge Torres Esquivel <jl.torres.es@gmail.com>
2015-10-22 15:13:19 -0500

    Interfaces Modificadas
    Actualización del estilo de la interfaz para profesores

--- public_html/js/pregunta.js
+++ public_html/js/pregunta.js
@@ -4,21 +4,61 @@ var respuestas = [];
 var miGrupo = "";
 
 $(document).ready(function(){
+        var miDescripcion,miOpA,miOpB,miOpC,miOpD,miRespuesta,miCategoria;
+        $("#alta").click(function(){
+            miDescripcion=$("#descrip").val();
+            miOpA=$("#op1").val();
+            miOpB=$("#op2").val();
+            miOpC=$("#op3").val();
+            miOpD=$("#op4").val();
+            miRespuesta=$("#respuesta").val();
+            miCategoria=$("#categoria").val();
+
+            if(miDescripcion != "" && miOpA != "" && miOpB != "" && miOpC != "" && miOpD != "" && miCategoria != ""){
+                respuestas[actual - 1] = {descripcion: miDescripcion,
+                            opcionA: miOpA,
+                            opcionB: miOpB,
+                            opcionC: miOpC,
+                            opcionD: miOpD,
+                            respuesta: miRespuesta,
+                            categoria: miCategoria};
+            
+                $.ajax({
+                    type: 'POST',
+                    url: '/nuevaPregunta',
+                    data: {grupo: miGrupo,
+                        resps: respuestas},
+                    success: function(data){
+                        if(data === "success"){
+                            alert("Preguntas guardada.");
+                            window.location = "agregarPregunta";
+                        }else{
+                            window.location = "entrada";
+                        }
+                    }
+                });
+            }else{
+                alert("Todos los campos son necesarios.");
+            }
+        });
+    });
+
+$(document).ready(function(){
         $("#acepta").click(function(){
             miGrupo=$("#grupo").val();
             preguntas = parseInt($("#noPreguntas").val());
             if(preguntas > 0){
                 $("#inicial").css('display','none');
-                var htmls = actual + " / " + preguntas + "<br />" +
-                        "Descripci&oacute;n:<br /><textarea id=\"descrip\" rows=\"5\"></textarea><br />" +
-                        "Opci&oacute;n 1:<br /><textarea id=\"op1\" rows=\"5\"></textarea><br />" +
-                        "Opci&oacute;n 2:<br /><textarea id=\"op2\" rows=\"5\"></textarea><br />" + 
-                        "Opci&oacute;n 3:<br /><textarea id=\"op3\" rows=\"5\"></textarea><br />" +
-                        "Opci&oacute;n 4:<br /><textarea id=\"op4\" rows=\"5\"></textarea><br />" +
-                        "Respuesta: <select id=\"respuesta\"><option value=\"1\">1</option>" +
-                        "<option value=\"2\">2</option><option value=\"3\">3</option>" +
-                        "<option value=\"4\">4</option></select><br />" +
-                        "Categor&iacute;a: <input type=\"text\" id=\"categoria\" maxlenght=\"50\" /><br />";
+                var htmls = "Pregunta : "+ actual + " / " + preguntas + "<br />" + "<div class='descripcionPregunta'> "+
+                        "<div class='descripcionPregunta' id='categoriaPregunta'>Categor&iacute;a:</div><input type=\"text\" id=\"categoria\" maxlenght=\"50\" /><br />"+
+                        "Descripci&oacute;n:"+"</div>"+"<textarea id=\"descrip\" rows=\"5\"></textarea><br />" +
+                        "<div class='descripcionPregunta' id='opcion1Text'>Opci&oacute;n 1:</div>"+"<br /><textarea id=\"op1\" rows=\"5\"></textarea><br />" +
+                        "<div class='descripcionPregunta' id='opcion2Text'>Opci&oacute;n 2:</div><br /><textarea id=\"op2\" rows=\"5\"></textarea><br />" + 
+                        "<div class='descripcionPregunta' id='opcion3Text'>Opci&oacute;n 3:</div><br /><textarea id=\"op3\" rows=\"5\"></textarea><br />" +
+                        "<div class='descripcionPregunta' id='opcion4Text'>Opci&oacute;n 4:</div><br /><textarea id=\"op4\" rows=\"5\"></textarea><br />" +
+                        "<div class='descripcionPregunta' id='res'>Respuesta:</div> <select id=\"respuesta\"><option value=\"1\">Opción 1</option>" +
+                        "<option value=\"2\">Opción 2</option><option value=\"3\">Opción 3</option>" +
+                        "<option value=\"4\">Opción 4</option></select><br />";
                 if(preguntas > 1){
                     htmls = htmls + "<input type=\"submit\" value=\"Siguiente\" id=\"siguiente\" onClick=\"next();\"/>";
                 }else{
@@ -137,17 +177,14 @@ function alta(){
                     respuesta: miRespuesta,
                     categoria: miCategoria};
 
-        var elJSON = JSON.stringify({preguntas: respuestas});
-
         $.ajax({
             type: 'POST',
             url: '/nuevaPregunta',
             data: {grupo: miGrupo,
-                resps: elJSON,
-                noPreguntas: preguntas},
+                resps: respuestas[0]},
             success: function(data){
                 if(data === "success"){
-                    alert("Preguntas guardadas.");
+                    alert("Preguntas guardada.");
                     window.location = "agregarPregunta";
                 }else{
                     window.location = "entrada";
